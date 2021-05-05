89661d081d31b84f34db6a6d740a079637a1e846
Sofian <74557858+Bsofian1@users.noreply.github.com>
2021-05-05 11:43:31 +0200

    update the info and change state checkbox

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Practical from "./components/Practical";
+import PersonalInfo from "./components/PersonalInfo";
+import EducationalXp from "./components/EducationalXp";
 
 function App() {
   return (
     <div className="App">
+      <h1 className="App-text">Hello sofian blue</h1>
+      <PersonalInfo />
+      <EducationalXp />
+      <Practical />
     </div>
   );
 }
